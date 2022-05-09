061f0ce82e4f0690a3a4522396a0c95e86f42398
PasqualCarstens <pasqualcarstens@gmail.com>
2022-05-09 16:37:38 +1000

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-      </header>
+      <Header />
     </div>
   );
 }
