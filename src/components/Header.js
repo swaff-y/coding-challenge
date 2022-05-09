66346a55f9e485b178d84ba73fd7a1d83fdf18e6
PasqualCarstens <pasqualcarstens@gmail.com>
2022-05-09 16:40:43 +1000

    Added logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Header.css";
+import logo from "../assets/logo.jpg";
 
 export default function Header() {
-  return <div className="Header">Header</div>;
+  return (
+    <div className="Header">
+      <img className="Logo" src={logo} alt="Logo" />
+    </div>
+  );
 }
