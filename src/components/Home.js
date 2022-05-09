1a474d4f331bbe2fd56543f4789d8b3cba1ab234
PasqualCarstens <pasqualcarstens@gmail.com>
2022-05-09 17:26:03 +1000

    Did the styling of the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
-  return <div className="Home">Home</div>;
+  return (
+    <div className="Home">
+      <h2 className="Text">Please choose an option from the navbar</h2>
+      <text>Games Won: X</text>
+      <button>Reset</button>
+    </div>
+  );
 }
